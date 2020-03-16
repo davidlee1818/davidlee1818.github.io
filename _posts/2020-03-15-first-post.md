cb0ba65485cf61b54b9bd0dd0530d39d37a8080c
davidlee1818 <davidlee1818@163.com>
2020-03-16 16:13:29 +0800

    fix linux black screen for better performance via reset winsock

--- _posts/2020-03-15-first-post.md
+++ _posts/2020-03-15-first-post.md
@@ -20,12 +20,25 @@ This post should display a **header with an overlay image**, if the theme suppor
 3. bundle install 提示找不到openssl
 
 ### 解决办法
-1. $> vim /etc/default/grub  <br/>
+1. <br />
+   - 方法一<br />
+   $> vim /etc/default/grub  <br/>
    增加nomodeset到GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash nomodeset\"  <br/>
    $> sudo update-grub  <br/>
    $> sudo reboot	<br/>
+   这个方法禁止虚拟机检测显卡，设置后屏幕可能很难看。
+   
+   - 方法二<br />
+   管理员cmd> netsh winsock reset   <br />
+   将winsock的目录重置为默认的设置状态。虚拟机与主机之间的通信方式是以socket进行的。
+   可能会因为某个程序通过LSP注入DLL,导致回收不彻底。影响虚拟的运行。
+   因此需要进行这些注入DLL的残留清理重置。 <br />
+   这个方法虚拟机显示效果更好。但是会重置防火墙设置 hosts文件变成只读
+
    有可能之前安装就一直黑屏，如果是vmware 建议用原生的模拟磁盘安装（选稍后自行安装），自动安装贼坑。
-   卡在retrieving file 可以 skip 不然等一年！
+   卡在retrieving file 可以 skip 不然等一年！ <br />
+   在安装完vmtool之后会黑屏
+    
 
 2. $> gem sources -l   <br/>
    $> gem sources -r   //加上上面的干掉官方源   <br/>
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Blog Format-Header Image Overlay",
-        "excerpt":"This post should display a header with an overlay image, if the theme supports it. 第一篇博客 为了用个jekyll模板，ubuntu折腾了一天。中间遇到一些bug贼痛苦，记录下来也许对别人有点用处。 坑点 Ubuntu持续黑屏 速度拉跨的gem源 bundle install 提示找不到openssl 解决办法 $&gt; vim /etc/default/grub 增加nomodeset到GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash nomodeset\" $&gt; sudo update-grub $&gt; sudo reboot 有可能之前安装就一直黑屏，如果是vmware 建议用原生的模拟磁盘安装（选稍后自行安装），自动安装贼坑。 卡在retrieving file 可以 skip 不然等一年！ $&gt; gem sources -l $&gt; gem sources...","categories": [],
+        "excerpt":"This post should display a header with an overlay image, if the theme supports it. 第一篇博客 为了用个jekyll模板，ubuntu折腾了一天。中间遇到一些bug贼痛苦，记录下来也许对别人有点用处。 坑点 Ubuntu持续黑屏 速度拉跨的gem源 bundle install 提示找不到openssl 解决办法 方法一 $&gt; vim /etc/default/grub 增加nomodeset到GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash nomodeset\" $&gt; sudo update-grub $&gt; sudo reboot 这个方法禁止虚拟机检测显卡，设置后屏幕可能很难看。 方法二 管理员cmd&gt; netsh winsock reset 将winsock的目录重置为默认的设置状态。虚拟机与主机之间的通信方式是以socket进行的。 可能会因为某个程序通过LSP注入DLL,导致回收不彻底。影响虚拟的运行。 因此需要进行这些注入DLL的残留清理重置。 这个方法虚拟机显示效果更好。但是会重置防火墙设置 hosts文件变成只读 有可能之前安装就一直黑屏，如果是vmware 建议用原生的模拟磁盘安装（选稍后自行安装），自动安装贼坑。...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/first-post/",
         "teaser": null
